ed2b8d6adfd3320063a55bc0602492d60a30ba1b
949027 <82878668+949027@users.noreply.github.com>
2022-09-10 16:03:17 +0700

    Models created

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, Tax, Discount, Item
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tax)
+class TaxAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    pass
--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,70 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField('Наименование', max_length=100)
+    description = models.TextField('Описание', blank=True)
+    price = models.DecimalField(
+        'Стоимость',
+        max_digits=8,
+        decimal_places=2,
+        validators=[MinValueValidator(0)],
+    )
+    #currency
+
+    def str(self):
+        return self.name
+
+
+class Discount(models.Model):
+    name = models.CharField('Наименование', max_length=100)
+    amount = models.DecimalField(
+        'Размер скидки',
+        max_digits=8,
+        decimal_places=2,
+        validators=[MinValueValidator(0)],
+    )
+
+    def str(self):
+        return self.name
+
+
+class Tax(models.Model):
+    name = models.CharField('Наименование', max_length=100)
+    amount = models.DecimalField(
+        'Размер налога',
+        max_digits=8,
+        decimal_places=2,
+        validators=[MinValueValidator(0)],
+    )
+
+    def str(self):
+        return self.name
+
+
+class Order(models.Model):
+    item = models.ManyToManyField(
+        Item,
+        verbose_name='Товар',
+        related_name='items',
+    )
+    discount = models.ManyToManyField(
+        Discount,
+        verbose_name='Скидка',
+        related_name='discounts',
+    )
+    tax = models.ManyToManyField(
+        Tax,
+        verbose_name='Налог',
+        related_name='taxes',
+    )
+    total_price = models.DecimalField(
+        'Общая стоимость',
+        max_digits=8,
+        decimal_places=2,
+        validators=[MinValueValidator(0)],
+    )
+
+    def str(self):
+        return self.pk
