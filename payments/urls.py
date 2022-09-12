905a21cc4945b305301c1dcece92dc3e56bcdd67
949027 <82878668+949027@users.noreply.github.com>
2022-09-12 22:44:48 +0700

    Added endpoints for API

--- payments/urls.py
+++ payments/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import get_payment_id, get_payment_page
 
 urlpatterns = [
-    path('buy/<int:item_id>', get_payment_id, name='get_payment_id'),
-    path('item/<int:item_id>', get_payment_page, name='get_payment_page'),
+    path('buy/<int:order_id>', get_payment_id, name='get_payment_id'),
+    path('order/<int:order_id>', get_payment_page, name='get_payment_page'),
 ]
--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,65 @@
+from contextlib import suppress
+import stripe
 from django.shortcuts import render
+from django.conf import settings
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from payments.models import Item, Order
+
+
+@api_view(['GET'])
+def get_payment_id(request, order_id):
+    discounts = []
+    taxes = []
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    with suppress(Item.DoesNotExist):
+        order = Order.objects.get(id=order_id)
+
+        if order.discount:
+            coupon = stripe.Coupon.create(percent_off=order.discount.amount, duration="once")
+            discounts.append({'coupon': coupon.id})
+
+        if order.tax:
+            tax = stripe.TaxRate.create(
+                display_name=order.tax.name,
+                inclusive=False,
+                percentage=order.tax.amount,
+            )
+            taxes.append(tax.id)
+
+        line_items = [
+            {
+                'price_data': {
+                    'currency': 'rub',
+                    'product_data': {
+                        'name': order_item.item.name,
+                    },
+                    'unit_amount': int(order_item.price * 100),
+                },
+                'quantity': order_item.quantity,
+                'tax_rates': taxes,
+            }
+            for order_item in order.order_items.all()
+        ]
+
+        session = stripe.checkout.Session.create(
+            line_items=line_items,
+            mode='payment',
+            discounts=discounts,
+            success_url='https://example.com/success',
+            cancel_url='https://example.com/cancel',
+        )
+        return Response(session.id)
+
+
+@api_view(['GET'])
+def get_payment_page(request, order_id):
+    with suppress(Item.DoesNotExist):
+        order = Order.objects.get(id=order_id)
+        context = {
+            'publishable_key': settings.STRIPE_PUBLISHABLE_KEY,
+            'order': order,
+            'items': order.order_items.all(),
+        }
+        return render(request, 'checkout.html', context=context)
