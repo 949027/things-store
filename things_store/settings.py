71de408903dad8bd17422f06fb16dde20cdb5ef2
949027 <82878668+949027@users.noreply.github.com>
2022-09-12 22:46:13 +0700

    Environment variables included. Database set up.

--- things_store/settings.py
+++ things_store/settings.py
@@ -9,8 +9,20 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+import dj_database_url
+from environs import Env
+
+
+env = Env()
+env.read_env()
+
+DB_URL = os.getenv('DB_URL')
+
+STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^d$bby%2#lm3=@3s$h&z1ei^bfbw2e@_zz_x4-4_t9#tzp6rk7'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,10 +88,12 @@ WSGI_APPLICATION = 'things_store.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv(
+            'DB_URL',
+            'sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+        )
+    )
 }
 
 
